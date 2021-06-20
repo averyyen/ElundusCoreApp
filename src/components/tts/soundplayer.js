6d73dd2fe3e658fc17bedc8184b2c3e09bf3b909
Avery Yen <averyyen@thisisartium.com>
2021-06-20 16:50:53 -0700

    Switch to h5 media player

--- src/components/tts/soundplayer.js
+++ src/components/tts/soundplayer.js
@@ -1,34 +1,34 @@
-import React from "react"
-import { connect } from "react-redux"
+import React from "react";
+import { connect } from "react-redux";
+import Card from "react-bootstrap/Card";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
 
-import Card from "react-bootstrap/Card"
-
-import Audioplayer from './audioplayer';
-
-import DownloadVoice from "./downloadvoice"
+import DownloadVoice from "./downloadvoice";
 
 class SoundPlayer extends React.Component {
-    render() {
-        if (this.props.voiceBlobUrl === '') {
-            return null;
-        }
-
-        return (
-            <Card>
-                <Card.Header>Result</Card.Header>
-                <Card.Body>
-                <Audioplayer src={this.props.voiceBlobUrl} />
-                    {/* <ShareVoice /> */}
-                    <DownloadVoice voiceBlobUrl={this.props.voiceBlobUrl} />
-                </Card.Body>
-            </Card>
-        )
+  render() {
+    if (this.props.voiceBlobUrl === "") {
+      return null;
     }
+
+    return (
+      <Card>
+        <Card.Header>Result</Card.Header>
+        <Card.Body>
+          <div className="audio-player">
+            <AudioPlayer autoPlay src={this.props.voiceBlobUrl} />
+          </div>
+          <DownloadVoice voiceBlobUrl={this.props.voiceBlobUrl} />
+        </Card.Body>
+      </Card>
+    );
+  }
 }
 
 export default connect((state, props) => {
-    return {
-        voiceBlobUrl: state.voiceBlobUrl,
-        voiceBlob: state.voiceBlob,
-    };
+  return {
+    voiceBlobUrl: state.voiceBlobUrl,
+    voiceBlob: state.voiceBlob,
+  };
 })(SoundPlayer);
